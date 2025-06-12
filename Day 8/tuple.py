0738374043cb8adb94282890a5150c4d27dab81f
Aditya008178 <waghaditya9923@gmail.com>
2025-06-12 15:39:43 +0530

    Day 9

---
+++ Day 8/tuple.py
@@ -0,0 +1,7 @@
+tup = ("Python","is","awesome")
+length =[]
+
+for word in tup:
+    length.append(len(word))
+     
+print(tuple(length))
---
+++ Home Assignment/Day 2/2 arithmetic operation using variables.py
@@ -0,0 +1,13 @@
+# 2.	Create a variable x = 10 and y = 5. Print the following:
+# 	Sum
+# 	Difference
+# 	Product
+# 	Quotient
+
+x = 10
+y = 5
+
+print(f"Addition: {x+y}")
+print(f"Diffrerence: {x-y}")
+print(f"Product: {x*y}")
+print(f"Quotient: {x/y}")
